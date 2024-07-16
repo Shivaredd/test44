f8e23495739216231bb6c0b3939f4261b4ed36ac
Shivaredd <35751968+Shivaredd@users.noreply.github.com>
2024-07-16 17:51:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 module "vpc" {
-  source = "git::git@github.com:Shivaredd/test44.git//modules/vpc?ref=main"
+  source = "git::git@github.com:Shivaredd/test17.git//modules/vpc?ref=main"
   cidr_block = "10.0.0.0/16"
 
 }
   module "subnets" {
-  source      = "git::git@github.com:Shivaredd/test44.git//modules/subnets?ref=main"
+  source      = "git::git@github.com:Shivaredd/test17.git//modules/subnets?ref=main"
   vpc_id = module.vpc.vpc_id
 }
 
   module "ec2" {
-  source       = "git::git@github.com:Shivaredd/test44.git//modules/ec2?ref=main"
+  source       = "git::git@github.com:Shivaredd/test17.git//modules/ec2?ref=main"
   subnet_id    = module.subnets.subnet_id
   ami_id       = "ami-0b684cc5e765f1d72"
   instance_type = "t3.micro"
