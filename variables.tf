6997600bc13e231046541b117b0bc2dfac7fd4ae
Shivaredd <35751968+Shivaredd@users.noreply.github.com>
2024-07-16 18:09:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,18 @@
 variable "cidr_block" {
   description = "The CIDR block for the VPC"
   type        = string
+  default = "10.0.0./16"
 }
 variable "vpc_id" {
   description = "The ID of the VPC"
   type        = string
+  default = "vpc-0c0229dff3dfb79f8"
 }
 
 variable "subnet_id" {
   description = "The ID of the subnet where the instance will be deployed"
   type        = string
+  default = "subnet-0a28ae2f90c628e6f"
 }
 
 variable "ami_id" {
